008b1cc962e9953bec0a5c2d8f3d7545554a4dad
Gadi <satwikagadi2005@gmail.com>
2026-04-09 11:48:14 +0530

    Fixed Mailer Code

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,19 +1,14 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  // Using the direct IPv4 host address for Gmail to prevent IPv6 errors
-  host: '74.125.137.108', 
-  port: 587,
-  secure: false, 
+  service: 'gmail',
+  port: 465,
+  secure: true, // This is mandatory for port 465
   auth: {
     user: process.env.ADMIN_EMAIL,
     pass: process.env.ADMIN_PASSWORD,
   },
-  // This forces Node to use IPv4 only
-  family: 4, 
   tls: {
-    // This allows the connection to proceed even if the IP address doesn't match the SSL name
-    servername: 'smtp.gmail.com',
     rejectUnauthorized: false
   },
   connectionTimeout: 20000 
