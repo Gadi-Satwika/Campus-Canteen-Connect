6fc312818950975e09c8e5ba875f7eaa44c2dd43
Gadi <satwikagadi2005@gmail.com>
2026-04-10 11:49:48 +0530

    Deployed Backend in Latest

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://campus-canteen-connect-production.up.railway.app/api'
+  baseURL: 'https://campus-canteen-connect-production-2564.up.railway.app/api'
 });
 
 export default API;
